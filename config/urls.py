de7786ebe50e655a38c286612f0a0dfc8c245173
Roberto West <roberto.west@gmail.com>
2019-01-30 15:25:00 -0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -13,7 +13,10 @@ from django.contrib.auth import views as auth_views
 urlpatterns += [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    # path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password/', 
+         auth_views.PasswordResetView.as_view(template_name='users/reset_pass.html'), 
+         name='reset_password'),    
 ]
 
 # agregamos acciones propias
@@ -25,6 +28,3 @@ urlpatterns += [
     path('profile/', views.profile, name='profile'),
     path('register/', views.register, name='register'),
 ]
-
-
-
